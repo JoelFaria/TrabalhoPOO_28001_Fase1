e95ad38a29b520f82c2e2aa1927a038bddada4f3
JoelFaria <a28001@alunos.ipca.pt>
2024-10-24 16:01:11 +0100

    Acrescento de subclasses

--- DLL_Classes/SubClasses.cs
+++ DLL_Classes/SubClasses.cs
@@ -2,8 +2,109 @@
 
 namespace TrabalhoPOO
 {
-    internal class SubClasses
+    public class Gpu
     {
-       
+        private int VRAM { get; set; }
+        private int BaseClock { get; set; }
+        private int BoostClock { get; set; }
+        private string MemoryType { get; set; }
+
+        public Gpu(int vram, int baseClock, int boostClock, string memoryType)
+        {
+            this.VRAM = vram;
+            this.BaseClock = baseClock;
+            this.BoostClock = boostClock;
+            this.MemoryType = memoryType;
+        }
+        public int GetVRAM
+        {
+            get { return VRAM; }
+            set { VRAM = value; }
+        }
+        public int GetBaseClock
+        {
+            get { return BaseClock; }
+            set { BaseClock = value; }
+        }
+        public int GetBoostClock
+        {
+            get { return BoostClock; }
+            set { BoostClock = value; }
+        }
+        public string GetMemoryType
+        {
+            get { return MemoryType; }
+            set { MemoryType = value; }
+        }
+    }
+    public class cpu
+    {
+        private int Cache { get; set; }
+        private string socket { get; set; }
+        private int MemorySupport { get; set; }
+        private int Frequency { get; set; }
+
+        public cpu(int cache, string socket, int memorySupport, int frequency)
+        {
+            this.Cache = cache;
+            this.socket = socket;
+            this.MemorySupport = memorySupport;
+            this.Frequency = frequency;
+        }
+        public int GetCache
+        {
+            get { return Cache; }
+            set { Cache = value; }
+        }
+        public string GetSocket
+        {
+            get { return socket; }
+            set { socket = value; }
+        }
+        public int GetMemorySupport
+        {
+            get { return MemorySupport; }
+            set { MemorySupport = value; }
+        }
+        public int GetFrequency
+        {
+            get { return Frequency; }
+            set { Frequency = value; }
+        }
+    }
+    public class Motherboard
+    {
+        private string socket { get; set; }
+        private string chipset { get; set; }
+        private int MemorySupport { get; set; }
+        private string FormFactor { get; set; }
+
+        public Motherboard(string socket, string chipset, int memorySupport, string formFactor)
+        {
+            this.socket = socket;
+            this.chipset = chipset;
+            this.MemorySupport = memorySupport;
+            this.FormFactor = formFactor;
+        }
+        public string GetSocket
+        {
+            get { return socket; }
+            set { socket = value; }
+        }
+        public string GetChipset
+        {
+            get { return chipset; }
+            set { chipset = value; }
+        }
+        public int GetMemorySupport
+        {
+            get { return MemorySupport; }
+            set { MemorySupport = value; }
+        }
+        public string GetFormFactor
+        {
+            get { return FormFactor; }
+            set { FormFactor = value; }
+        }
     }
 }
